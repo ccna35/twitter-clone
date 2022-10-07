0457834c7fbf880ce61819b6ebb907cbc9187049
ccna35 <shawkykhalil@gmail.com>
2022-10-07 21:40:03 +0200

    "Updated chat box UI"

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -3,7 +3,7 @@ import { BiHomeCircle } from "react-icons/bi";
 import { RiSearchLine } from "react-icons/ri";
 import { BsBell, BsArrowDownSquare } from "react-icons/bs";
 import { IoMailOutline } from "react-icons/io5";
-import { FiArrowDown } from "react-icons/fi";
+import { FiArrowDown, FiArrowUp } from "react-icons/fi";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFeather } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
@@ -11,21 +11,76 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   ChatBoxBar,
   ChatBoxIconContainer,
+  ChatBoxIconsContainer,
   ChatBoxStyle,
+  ChatsContainer,
+  Conversation,
+  UserInfoMessageContainer,
 } from "./styles/ChatBox.styled";
+import { useState } from "react";
+import { UserPhoto, UserPhotoContainer } from "./styles/NewTweet.styled";
+import {
+  TweetAuthor,
+  TweetInfoContainer,
+  UserHandle,
+  UserName,
+  UserVerifiedIconContainer,
+} from "./styles/Tweet.styled";
+import { MdVerified } from "react-icons/md";
 
 function ChatBox() {
   const { user } = useSelector((state) => state.auth);
 
+  const [boxVisible, setBoxVisible] = useState(false);
+
   return (
-    <ChatBoxStyle>
-      <ChatBoxBar>
+    <ChatBoxStyle visible={boxVisible}>
+      <ChatBoxBar onClick={() => setBoxVisible((prev) => !prev)}>
         <h2>Messages</h2>
-        <ChatBoxIconContainer>
-          <IoMailOutline size="1.5rem" />
-          <FiArrowDown size="1.5rem" />
-        </ChatBoxIconContainer>
+        <ChatBoxIconsContainer>
+          <ChatBoxIconContainer>
+            <IoMailOutline size="1.2rem" />
+          </ChatBoxIconContainer>
+          <ChatBoxIconContainer>
+            {boxVisible ? (
+              <FiArrowUp size="1.5rem" />
+            ) : (
+              <FiArrowDown size="1.5rem" />
+            )}
+          </ChatBoxIconContainer>
+        </ChatBoxIconsContainer>
       </ChatBoxBar>
+      <ChatsContainer visible={boxVisible}>
+        <Conversation>
+          <UserPhotoContainer>
+            <UserPhoto
+              src={"./images/blank-profile-picture-gf8e58e24f_640.png"}
+            />
+          </UserPhotoContainer>
+          <UserInfoMessageContainer>
+            <TweetInfoContainer>
+              <UserName>
+                <TweetAuthor>Mike Adams</TweetAuthor>
+                <UserVerifiedIconContainer>
+                  <MdVerified />
+                </UserVerifiedIconContainer>
+              </UserName>
+              <UserHandle>@mike_adams</UserHandle>
+            </TweetInfoContainer>
+            <p>This is amazing I must say, well done man 👏</p>
+          </UserInfoMessageContainer>
+        </Conversation>
+        <Conversation>
+          <h2>User</h2>
+          <p>Chat text</p>
+          <p>Chat date</p>
+        </Conversation>
+        <Conversation>
+          <h2>User</h2>
+          <p>Chat text</p>
+          <p>Chat date</p>
+        </Conversation>
+      </ChatsContainer>
     </ChatBoxStyle>
   );
 }
--- client/src/components/styles/ChatBox.styled.js
+++ client/src/components/styles/ChatBox.styled.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const ChatBoxStyle = styled.div`
-  width: 21rem;
-  height: 30rem;
+  width: 25rem;
   position: fixed;
   right: 2rem;
   bottom: 0;
@@ -11,8 +10,12 @@ export const ChatBoxStyle = styled.div`
   border-top-right-radius: 1rem;
   box-shadow: 0px 0px 3px 1px #dde0e3;
   overflow-x: hidden;
-  overflow-y: scroll;
-  /* @media (min-width: ${({ theme }) => theme.breakpoints.xxs}) {
+
+  ${(props) =>
+    props.visible
+      ? "overflow-y: hidden"
+      : "overflow-y: scroll"}/* @media (min-width: ${({ theme }) =>
+    theme.breakpoints.xxs}) {
     display: none;
   } */
 `;
@@ -30,7 +33,49 @@ export const ChatBoxBar = styled.div`
     font-weight: 400;
   }
 `;
-export const ChatBoxIconContainer = styled.div`
+export const ChatBoxIconsContainer = styled.div`
   display: flex;
+  gap: 0.5rem;
+`;
+export const ChatBoxIconContainer = styled.div`
+  display: grid;
+  place-items: center;
+  border-radius: 50%;
+  /* padding: 1rem; */
+  width: 2rem;
+  height: 2rem;
+  transition: background-color 0.5s;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.LeftMenuBgHoverColor};
+  }
+`;
+
+export const ChatsContainer = styled.div`
+  width: 100%;
+  max-height: 100%;
+  /* background-color: aliceblue; */
+  /* transition: max-height 0.5s; */
+
+  ${(props) => props.visible && "display: none;"}
+`;
+export const Conversation = styled.div`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 1rem;
+  padding-right: 1rem;
   gap: 1rem;
+  border-bottom: 1px solid lightgray;
+`;
+export const UserInfoMessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  p {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: 12rem;
+  }
 `;
